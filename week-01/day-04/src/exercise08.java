05c3279349fa87c280998cc91ba88bcd3812cdd3
L0riana <jserly@gmail.com>
2018-02-15 16:44:28 +0100

    feat(exercise08)changed

--- week-01/day-04/src/exercise08.java
+++ week-01/day-04/src/exercise08.java
@@ -3,9 +3,10 @@ public class exercise08 {
         // Swap the values of the variables
         int a = 123;
         int b = 526;
+        int temp = a;
 
-        a = 526;
-        b = 123;
+        a = b;
+        b = temp;
 
         System.out.println(a);
         System.out.println(b);
