b8e662ff6e1dfaa6753875188303caaabdc89c7a
L0riana <jserly@gmail.com>
2018-02-16 11:57:08 +0100

    feat(exercise15)added

---
+++ week-01/day-05/src/exercise15.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class exercise15 {
+    public static void main(String[] args) {
+        // Write a program that asks for an integer that is a distance in kilometers,
+        // then it converts that value to miles and prints it
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, write a whole number here: ");
+
+        int distance = scanner.nextInt();
+        double mile;
+
+        mile = distance * 0.62137;
+
+        System.out.println(distance + " kilometer(s) equals to: " + mile + " mile(s).");
+    }
+}
