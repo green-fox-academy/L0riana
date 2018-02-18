c93c19a3112cc987bbb1aceaf83afa11856ca913
L0riana <jserly@gmail.com>
2018-02-18 22:20:36 +0100

    feat(exercise28)added

---
+++ week-01/day-05/src/exercise28.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class exercise28 {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // triangle like this:
+        //
+        // *
+        // **
+        // ***
+        // ****
+        //
+        // The triangle should have as many lines as the number was
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number here: ");
+        int number = scanner.nextInt();
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 0; j<i; j++) {
+                System.out.print("*");
+            } System.out.println("");
+        }
+    }
+}
