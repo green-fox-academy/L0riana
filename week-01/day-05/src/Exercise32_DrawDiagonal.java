8efd1b4089b4e968ccc398ad17637bc5f47766c0
L0riana <jserly@gmail.com>
2018-02-18 23:12:03 +0100

    feat(exercise28)added

---
+++ week-01/day-05/src/Exercise32_DrawDiagonal.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Exercise32_DrawDiagonal {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // square like this:
+        //
+        //
+        // %%%%%
+        // %%  %
+        // % % %
+        // %  %%
+        // %%%%%
+        //
+        // The square should have as many lines as the number was
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number here: ");
+        int number = scanner.nextInt();
+
+        for (int i = 0; i < number; i++) {
+            System.out.print("%");
+        }
+        System.out.println();
+        for (int j = 1; j < number-1; j++) {
+            for (int k = 0; k <= number; k++) {
+                if ((k == 0) || (j == k) || (k == number-1)) {
+                    System.out.print("%");
+                } else {
+                    System.out.print(" ");
+                }
+            } System.out.println();
+        }
+        for (int i = 0; i < number; i++) {
+            System.out.print("%");
+        }
+    }
+}
