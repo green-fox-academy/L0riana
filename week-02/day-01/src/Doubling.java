f514ec1f6e885cb2a7d2d2ed969ab712cb30a5ab
L0riana <jserly@gmail.com>
2018-02-19 12:30:10 +0100

    feat(Doubling)added

---
+++ week-02/day-01/src/Doubling.java
@@ -0,0 +1,14 @@
+public class Doubling {
+    public static void main(String[] args) {
+// - Create an integer variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// - Print the result of `doubling(ak)`
+        int ak = 123;
+        doubling(ak);
+
+    } public static void doubling(int callAk){
+        callAk = callAk * 2;
+        System.out.println(callAk);
+    }
+
+}
