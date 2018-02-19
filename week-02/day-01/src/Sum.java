fc9be3efad1f579b5daa0c062a0443a232b6136b
L0riana <jserly@gmail.com>
2018-02-19 15:44:14 +0100

    feat(Factorio)added

--- week-02/day-01/src/Sum.java
+++ week-02/day-01/src/Sum.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Sum {
+// - Write a function called `sum` that sum all the numbers
+//   until the given parameter and returns with an integer
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Numbers will be added from 1 to your number to get the sum. \nEnter your number here: ");
