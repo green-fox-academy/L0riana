b0b18e400d5558e488f1eba35f2b45f156cd88be
L0riana <jserly@gmail.com>
2018-02-21 13:25:27 +0100

    feat(CenterBoxSquare) complete

---
+++ week-02/day-03/src/CenterBoxSquare.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class CenterBoxSquare {
+  public static void mainDraw(Graphics graphics) {
+    // create a square drawing function that takes 1 parameter:
+    // the square size
+    // and draws a square of that size to the center of the canvas.
+    // draw 3 squares with that function.
+    // avoid code duplication.
+
+
+    int size = 10;
+
+    drawRectangles(graphics, size);
+
+  }
+
+  private static void drawRectangles(Graphics graphics, int size) {
+    int x = WIDTH/2;
+    int y = HEIGHT/2;
+    for (int i = 0; i < 4; i++) {
+      graphics.drawRect(x, y, 20, 20);
+      y = y + 20;
+  }
+
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 343;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
