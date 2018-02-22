23fb40857e518e122949ce117b7b1fb629fcec8f
L0riana <jserly@gmail.com>
2018-02-22 10:17:36 +0100

    style(Printer)

--- week-02/day-01/src/Printer.java
+++ week-02/day-01/src/Printer.java
@@ -1,21 +1,22 @@
 public class Printer {
-// - Create a function called `printer`
-//   which prints the input String parameters
-// - It can have any number of parameters
 
-// Examples
-// printer("first")
-// printer("first", "second")
-// printer("first", "second", "third", "fourh")
-// ...
-        public static void main(String[] args){
-            printer("apple", "monkey", "dog", "rabbit");
-        }
-        static void printer(String...words){
-            System.out.print("printer(");
-            for (String word : words) {
-                System.out.print("\"" + word + "\", ");
-            }
-            System.out.print(")");
-        }
+  public static void main(String[] args) {
+    // - Create a function called `printer`
+    //   which prints the input String parameters
+    // - It can have any number of parameters
+
+    // Examples
+    // printer("first")
+    // printer("first", "second")
+    // printer("first", "second", "third", "fourth")
+    // ...
+    String[] words = {"apple", "monkey", "dog", "rabbit"};
+    printer(words);
+  }
+
+  static void printer(String... words) {
+    System.out.print("printer(");
+    for (String word : words) System.out.print("\"" + word + "\", ");
+    System.out.print(")");
+  }
 }
