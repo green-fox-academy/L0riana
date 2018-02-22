f4d391989f4fe5216edbe4c9141428cdecd4f57d
L0riana <jserly@gmail.com>
2018-02-22 12:27:30 +0100

    style(SolarSystem, Matchmaking, AppendLetter) indentation

--- week-02/day-02/src/Matchmaking.java
+++ week-02/day-02/src/Matchmaking.java
@@ -1,22 +1,21 @@
 import java.util.*;
 
 public class Matchmaking {
-    public static void main(String... args) {
-        ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
-        ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
+  public static void main(String... args) {
+    ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
+    ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
 
-        // Write a method that joins the two lists by matching one girl with one boy into a new list
-        // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+    // Write a method that joins the two lists by matching one girl with one boy into a new list
+    // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
 
-        System.out.println(makingMatches(girls, boys));
+    System.out.println(makingMatches(girls, boys));
+  }
 
+  public static ArrayList<String> makingMatches(ArrayList<String> girlsToMatch, ArrayList<String> boysToMatch) {
+    ArrayList<String> newList = new ArrayList<>();
+    for (int i = 0; i < girlsToMatch.size(); i++) {
+      newList.add("\"" + girlsToMatch.get(i) + "\", " + "\"" + boysToMatch.get(i) + "\"");
     }
-
-    public static ArrayList<String> makingMatches(ArrayList<String> girlsToMatch, ArrayList<String> boysToMatch) {
-        ArrayList<String> newList = new ArrayList<>();
-        for (int i = 0; i < girlsToMatch.size(); i++) {
-            newList.add("\"" + girlsToMatch.get(i) + "\", " + "\"" + boysToMatch.get(i) + "\"");
-        }
-        return newList;
-    }
+    return newList;
+  }
 }
--- week-02/day-02/src/SolarSystem.java
+++ week-02/day-02/src/SolarSystem.java
@@ -2,17 +2,17 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class SolarSystem {
-    public static void main(String... args) {
-        ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"));
-        // Saturn is missing from the planetList
-        // Insert it into the correct position
-        // Create a method called putSaturn() which has list parameter and returns the correct list.
-        System.out.println(putSaturn(planetList));
-        // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
-    }
+  public static void main(String... args) {
+    ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"));
+    // Saturn is missing from the planetList
+    // Insert it into the correct position
+    // Create a method called putSaturn() which has list parameter and returns the correct list.
+    System.out.println(putSaturn(planetList));
+    // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+  }
 
-    public static ArrayList<String> putSaturn(ArrayList<String> newPlanetList) {
-        newPlanetList.add("Saturn");
-        return newPlanetList;
-    }
+  public static ArrayList<String> putSaturn(ArrayList<String> newPlanetList) {
+    newPlanetList.add("Saturn");
+    return newPlanetList;
+  }
 }
