e65ee8aa5c189dbff2d91a99775c1a67c12e11df
L0riana <jserly@gmail.com>
2018-02-23 10:43:37 +0100

    feat(LinePlayDivided) init

---
+++ week-02/day-03/src/LinePlayDivided.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LinePlayDivided {
+  public static void mainDraw(Graphics graphics) {
+
+    drawLines(graphics);
+
+  }
+
+  private static void drawLines(Graphics graphics) {
+    int x = 15; //number of lines per side
+    int n = 2;
+    for (int i = 0; i < x; i++) {
+      graphics.setColor(new Color(153, 0, 153));
+      graphics.drawLine(i * WIDTH / n / x, 0, WIDTH / n, (i + 1) * WIDTH / n / x);
+      graphics.drawLine(WIDTH / n + i * WIDTH / x / n, 0, WIDTH, (i + 1) * WIDTH / n / x);
+      graphics.drawLine(i * WIDTH / n / x, WIDTH / n, WIDTH / n, (i + 1) * WIDTH / n / x + WIDTH / n);
+      graphics.drawLine(WIDTH / n + i * WIDTH / x / n, 0 + WIDTH / n, WIDTH, (i + 1) * WIDTH / n / x + WIDTH / n);
+      graphics.setColor(Color.GREEN);
+      graphics.drawLine(0, i * WIDTH / n / x, (i + 1) * WIDTH / n / x, WIDTH / n);
+      graphics.drawLine(WIDTH / n, i * WIDTH / n / x, WIDTH / n + (i + 1) * WIDTH / n / x, WIDTH / n);
+      graphics.drawLine(0, WIDTH / n + i * WIDTH / n / x, (i + 1) * WIDTH / n / x, WIDTH);
+      graphics.drawLine(WIDTH / n, WIDTH / n + i * WIDTH / n / x, WIDTH / n + (i + 1) * WIDTH / n / x, WIDTH);
+    }
+  }
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 343;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
