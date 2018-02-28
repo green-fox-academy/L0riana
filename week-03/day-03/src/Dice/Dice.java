2ad83219fc72f4e169a462efe9a0d76714c89369
L0riana <jserly@gmail.com>
2018-02-28 15:50:01 +0100

    feat(Dice) add

---
+++ week-03/day-03/src/Dice/Dice.java
@@ -0,0 +1,65 @@
+package Dice;
+
+import java.util.Arrays;
+
+public class Dice {
+  //    You have a Dice class which has 6 dice
+  //    You can roll all of them with roll()
+  //    Check the current rolled numbers with getCurrent()
+  //    You can reroll with reroll()
+  //    Your task is to get where all dice is a 6
+  int[] dices = new int[6];
+
+  public static void main(String[] args) {
+    Dice myDice = new Dice();
+
+    myDice.roll();
+    myDice.getCurrent();
+    myDice.reroll();
+
+    System.out.println("First 6 roll: " + myDice.toString());
+
+    int countRound = 0;
+
+    for (int i = 0; i < myDice.dices.length; i++) {
+      while (myDice.getCurrent(i) != 6){
+        myDice.reroll(i);
+        System.out.println("Check values in each round: " + myDice.toString());
+        countRound++;
+      }
+    }
+    System.out.println("Check if all values are 6: " + myDice.toString());
+    System.out.println("Reroll nr: " + countRound);
+  }
+
+  public int[] roll() {
+    for (int i = 0; i < dices.length; i++) {
+      dices[i] = (int) (Math.random() * 6) + 1;
+    }
+    return dices;
+  }
+
+  public int[] getCurrent() {
+    return dices;
+  }
+
+  public int getCurrent(int i) {
+    return dices[i];
+  }
+
+  public void reroll() {
+    for (int i = 0; i < dices.length; i++) {
+      dices[i] = (int) (Math.random() * 6) + 1;
+    }
+  }
+
+  public void reroll(int k) {
+    dices[k] = (int) (Math.random() * 6) + 1;
+  }
+  @Override
+  public String toString() {
+    return "Dice{" +
+            "dices = " + Arrays.toString(dices) +
+            '}';
+  }
+}
