f0e7692d166e5735e1436748f2574a8609b9befb
L0riana <jserly@gmail.com>
2018-03-01 16:01:56 +0100

    feat(String2) add

---
+++ week-03/day-04/src/String2.java
@@ -0,0 +1,20 @@
+public class String2 {
+  public static void main(String[] args) {
+    // Given a string, compute recursively a new string where all the 'x' chars have been removed.
+    String text = "xyoxlkx is xyellxxow";
+    char remove = 'x';
+
+    System.out.println(text);
+    String newString = correctString(text, remove);
+    System.out.println(newString);
+  }
+
+  public static String correctString(String text, char delete) {
+    if (text.length() < 1)
+      return text;
+    if (text.charAt(0) == delete) {
+      text = text.substring(1);
+    }
+    return text.charAt(0) + correctString(text.substring(1, text.length()), delete);
+  }
+}
