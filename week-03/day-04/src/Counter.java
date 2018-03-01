add0bc417854fe9aad5de81f3e2bf4d11dde1f30
L0riana <jserly@gmail.com>
2018-03-01 10:41:26 +0100

    feat(Counter) add

---
+++ week-03/day-04/src/Counter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Counter {
+
+  public static void main(String[] args) {
+    // Write a recursive function that takes one parameter: n and counts down from n.
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your number here: ");
+    int number = scanner.nextInt();
+    System.out.println(counter(number));
+  }
+
+  private static int counter(int n) {
+    if (n == 0) {
+      return 0;
+    } else {
+      if (n == n);
+      System.out.println(n);
+      return counter(n - 1);
+    }
+  }
+}
