edfe1aec11178209bfd1cf9fb3a311150d49b8bd
L0riana <jserly@gmail.com>
2018-03-06 12:17:41 +0100

    style(greenfox) minor changes

--- week-04/day-02/src/greenfox/Person.java
+++ week-04/day-02/src/greenfox/Person.java
@@ -24,28 +24,4 @@ public class Person {
   public void getGoal() {
     System.out.println("My goal is: Live for the moment!");
   }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
-
-  public String getGender() {
-    return gender;
-  }
-
-  public void setGender(String gender) {
-    this.gender = gender;
-  }
 }
