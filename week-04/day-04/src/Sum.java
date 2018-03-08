dee9e02ca0ceca94b91f1d00849817954250e2d2
L0riana <jserly@gmail.com>
2018-03-08 12:46:30 +0100

    feat(Sum) complete

---
+++ week-04/day-04/src/Sum.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sum {
+
+  ArrayList<Integer> list1;
+
+  public Sum(){
+    this.list1 = new ArrayList<>();
+  }
+
+  public int sumListElements(List<Integer> list1){
+    int sum = 0;
+    for (int i = 0; i < list1.size(); i++) {
+      sum += list1.get(i);
+    }
+    return sum;
+  }
+}
+
---
+++ week-04/day-04/tests/SumTest.java
@@ -0,0 +1,43 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class SumTest {
+
+  Sum sumTest = new Sum();
+  ArrayList<Integer> list1 = new ArrayList<>();
+
+  @Test
+  public void sumTestEmptyList() {
+    assertEquals(0, sumTest.sumListElements(list1));
+  }
+
+  @Test
+  public void sumTestOneElement() {
+    list1.add(1);
+    assertEquals(1, sumTest.sumListElements(list1));
+  }
+
+  @Test
+  public void sumTestMultipleElements() {
+    list1.add(1);
+    list1.add(2);
+    assertEquals(3, sumTest.sumListElements(list1));
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void sumTestNull() {
+    list1.add(null);
+    assertEquals(6, sumTest.sumListElements(list1));
+  }
+
+  @Test
+  public void sumTestList1() {
+    for (int i = 1; i < 4; i++) {
+      list1.add(i);
+    }
+    assertEquals(6, sumTest.sumListElements(list1));
+  }
+}
