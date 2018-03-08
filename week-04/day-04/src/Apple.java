619867621e285ae2354dc58c818c05a7e8f9dcb0
L0riana <jserly@gmail.com>
2018-03-08 11:16:57 +0100

    feat(Apple) complete

---
+++ week-04/day-04/src/Apple.java
@@ -0,0 +1,12 @@
+public class Apple {
+
+  private String apple;
+
+  public Apple(String apple) {
+    this.apple = apple;
+  }
+
+  public String getApple() {
+    return apple;
+  }
+}
---
+++ week-04/day-04/tests/AppleTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppleTest {
+
+  private Apple apple = new Apple("apple");
+
+  @Test
+  public void getAppleTest() {
+    assertEquals("apple", apple.getApple());
+  }
+}
