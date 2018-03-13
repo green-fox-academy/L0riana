89f9f439acbf97255b6bf17f6b61d8d2a83ce7a6
L0riana <jserly@gmail.com>
2018-03-13 10:16:40 +0100

    practice

---
+++ practice/src/Crashes.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Crashes {
+  public static void main(String[] args) {
+    // The crash-incidents.csv file contains data of road crash-incidents.
+    // (id; location; surface type; surface condition; light condition; weather condition; crash date; year)
+    // Your task will be to count how many crash-incidents happened at good/bad
+    // weather conditions.
+    // Good weather conditions are CLOUDY and CLEAR.
+    // Bad weather conditions are RAIN, FREEZING RAIN, SNOW, FOG and SEVERE CROSSWINDS.
+    // So create the necessary method(s) that returns with the amount of crash-incidents.
+    // And print the return value to the console:
+    // The amount of crashes at good weather conditions: {amount}
+    // The amount of crashes at bad weather conditions: {amount}
+    Path pathCrashIncidents = Paths.get("crash-incidents.csv");
+    System.out.println(weatherCount(pathCrashIncidents));;
+  }
+  
+  public static String weatherCount(Path pathCrashIncidents) {
+    int countGood = 0;
+    int countBad = 0;
+    List<String> lines = null;
+    try {
+      lines = Files.readAllLines(pathCrashIncidents);
+      for (String line : lines) {
+        if (line.contains("CLOUDY") ||
+                (line.contains("CLEAN"))) {
+          countGood++;
+        } else if (line.contains("RAIN") ||
+                (line.contains("FREEZING RAIN") ||
+                        (line.contains("SNOW") ||
+                                (line.contains("FOG") ||
+                                        (line.contains("SEVERE CROSSWINDS")))))) {
+          countBad++;
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    System.out.println(lines.size());
+    return countGood + ", " + countBad;
+  }
+}
