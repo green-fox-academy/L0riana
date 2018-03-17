d557b2c1c0334d62811d17140fe5e31d66bf7fbc
L0riana <jserly@gmail.com>
2018-03-17 14:11:06 +0100

    feat(countLines) corrected

--- week-03/day-02/src/CountLines.java
+++ week-03/day-02/src/CountLines.java
@@ -1,42 +1,31 @@
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class CountLines {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     // Write a function that takes a filename as string,
     // then returns the number of lines the file contains.
     // It should return zero if it can't open the file, and
     // should not raise any error.
-    countLines();
+    countLines("my-file.txt");
   }
 
-  private static void countLines() throws IOException {
-    ArrayList<String> list = new ArrayList<>(Arrays.asList("dog", "cat", "mouse"));
-    String pathString = "my-file.txt";
-    Path path1 = Paths.get(pathString);
-    File file1 = new File(pathString);
-    int i = 0;
+  private static int countLines(String file) {
+
+    Path path1 = Paths.get(file);
+    int lineNumber = 0;
+
     try {
-      if (!file1.exists()) {
-        System.out.println("Unable to read file: my-file.txt. It will be created.");
-        file1.createNewFile();
-      }
-      Files.write(path1, list);
       for (String line : Files.readAllLines(path1)) {
-        i++;
+        lineNumber++;
       }
-      System.out.println("The file contains " + i + " lines.");
+      System.out.println("The file contains " + lineNumber + " line(s).");
+      return lineNumber;
     } catch (IOException e) {
-      e.printStackTrace();
-
-    } finally {
-
+      System.out.println("The file does not exist.");
+      return 0;
     }
   }
 }
