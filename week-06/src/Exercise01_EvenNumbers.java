77ae883d7514c8c9d4994d4bc0935db1556a9b05
L0riana <jserly@gmail.com>
2018-03-26 12:24:36 +0200

    feat(stream) week-06 excercise 01 with method

--- week-06/src/Exercise01_EvenNumbers.java
+++ week-06/src/Exercise01_EvenNumbers.java
@@ -7,11 +7,24 @@ public class Exercise01_EvenNumbers {
   public static void main(String[] args) {
     //Write a Stream Expression to get the even numbers from the following array:
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+    //with method
+    evenNumbers(numbers);
+
+    // with stream
     List<Integer> evenNumbers = numbers
             .stream()
             .filter(n -> n % 2 == 0)
             .collect(Collectors.toList());
 
     System.out.println(evenNumbers);
+  }
+
+  public static void evenNumbers(ArrayList<Integer> numbers) {
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers.get(i) % 2 == 0){
+        System.out.println(numbers.get(i));
+      }
+    }
   }
 }
