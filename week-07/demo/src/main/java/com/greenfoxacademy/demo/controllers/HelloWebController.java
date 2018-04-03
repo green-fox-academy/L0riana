4d21e418c412b6f2c39ce95821672a9a04903d6c
L0riana <jserly@gmail.com>
2018-04-03 17:11:25 +0200

    feat HelloWebController counter

--- week-07/demo/src/main/java/com/greenfoxacademy/demo/controllers/HelloWebController.java
+++ week-07/demo/src/main/java/com/greenfoxacademy/demo/controllers/HelloWebController.java
@@ -3,17 +3,20 @@ package com.greenfoxacademy.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
 
-  private AtomicLong id = new AtomicLong();
+  private AtomicLong count = new AtomicLong();
 
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam("name") String name) {
+    model.addAttribute("name", name);
+    count.getAndIncrement();
+    model.addAttribute("count", count);
     return "greeting";
   }
 }
