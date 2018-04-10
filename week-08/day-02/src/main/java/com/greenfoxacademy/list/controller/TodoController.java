72c23f3090ff70a73d32f8918f8a60b584dbaa9b
L0riana <jserly@gmail.com>
2018-04-10 13:26:57 +0200

    feat(TodoApp) add todos, connect to database

--- week-08/day-02/src/main/java/com/greenfoxacademy/list/controller/TodoController.java
+++ week-08/day-02/src/main/java/com/greenfoxacademy/list/controller/TodoController.java
@@ -1,18 +1,22 @@
-package com.greenfoxacademy.list;
+package com.greenfoxacademy.list.controller;
 
+import com.greenfoxacademy.list.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/todo")
 public class TodoController {
 
+  @Autowired
+  TodoRepository todoRepository;
+
   @GetMapping({"/", "/list"})
-  @ResponseBody
   public String list(Model model){
-    return "This is my first todo";
+    model.addAttribute("todos", todoRepository.findAll());
+    return "todolist";
   }
 }
