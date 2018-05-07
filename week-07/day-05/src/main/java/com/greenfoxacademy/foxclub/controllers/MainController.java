99f9a468abd2d78590b2adb3a4beb0c64d54244c
L0riana <jserly@gmail.com>
2018-05-07 11:16:39 +0200

    feat(greenfox) main and login pages

--- week-07/day-05/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
+++ week-07/day-05/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
@@ -1,14 +1,25 @@
 package com.greenfoxacademy.foxclub.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-  @RequestMapping(value = "/")
+  @GetMapping(value = "/")
   public String indexPage(@RequestParam(name = "name") String name) {
     return "index";
+  }
+
+  @GetMapping("/login")
+  public String loginPage() {
+    return "login";
+  }
+
+  @PostMapping("/login")
+  public String login(@RequestParam(name = "name") String name) {
+    return "redirect:/?name=" + name;
   }
 }
