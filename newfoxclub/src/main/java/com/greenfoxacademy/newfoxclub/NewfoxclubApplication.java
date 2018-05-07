bb4980d541c8567458520fc72c950e80b374545e
L0riana <jserly@gmail.com>
2018-05-07 14:47:58 +0200

    newfoxclub init

---
+++ newfoxclub/src/main/java/com/greenfoxacademy/newfoxclub/NewfoxclubApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.newfoxclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewfoxclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewfoxclubApplication.class, args);
+	}
+}
---
+++ newfoxclub/src/test/java/com/greenfoxacademy/newfoxclub/NewfoxclubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.newfoxclub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewfoxclubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
