77281227a88c5efe2d389f25e7ab89b99ce31bcf
L0riana <jserly@gmail.com>
2018-05-11 00:40:28 +0200

    todoRestApi in-memory database H2 dependency

---
+++ week-11/todo_rest_api/src/test/java/com/greenfoxacademy/todo_rest_api/controllers/ApplicationUserRestControllerTest.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.todo_rest_api.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationUserRestControllerTest {
+
+  @Test
+  public void login() {
+  }
+}
