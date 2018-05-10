3e447b4cc4e094fc80ca03534158154a13e4a261
L0riana <jserly@gmail.com>
2018-05-10 11:21:05 +0200

    securitydemo GET endpoints security dependency

---
+++ week-11/securitydemo/src/main/java/com/greenfoxacademy/securitydemo/endpoint/FirstFloorEndpoint.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy.securitydemo.endpoint;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/floor1")
+public class FirstFloorEndpoint {
+
+    @GetMapping("office1")
+    public ResponseEntity enterOffice1(){
+        return new ResponseEntity("You are inside office 1", HttpStatus.OK);
+    }
+
+    @GetMapping("office2")
+    public ResponseEntity enterOffice2(){
+        return new ResponseEntity("You are inside office 2", HttpStatus.OK);
+    }
+}
