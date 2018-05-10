813ca78e6ddbacee5066c2e77d85e367ca1649e0
L0riana <jserly@gmail.com>
2018-05-10 12:57:26 +0200

    service, config classes

--- week-11/securitydemo/src/main/java/com/greenfoxacademy/securitydemo/endpoint/FirstFloorEndpoint.java
+++ week-11/securitydemo/src/main/java/com/greenfoxacademy/securitydemo/endpoint/FirstFloorEndpoint.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.securitydemo.endpoint;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1/floor1")
 public class FirstFloorEndpoint {
 
-    @GetMapping("office1")
-    public ResponseEntity enterOffice1(){
-        return new ResponseEntity("You are inside office 1", HttpStatus.OK);
-    }
+  @GetMapping("office1")
+  @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+  public ResponseEntity enterOffice1() {
+    return new ResponseEntity("You are inside office 1", HttpStatus.OK);
+  }
 
-    @GetMapping("office2")
-    public ResponseEntity enterOffice2(){
-        return new ResponseEntity("You are inside office 2", HttpStatus.OK);
-    }
+  @GetMapping("office2")
+  @PreAuthorize("hasAnyRole('ADMIN')")
+  public ResponseEntity enterOffice2() {
+    return new ResponseEntity("You are inside office 2", HttpStatus.OK);
+  }
 }
--- week-11/securitydemo/src/main/java/com/greenfoxacademy/securitydemo/security/SecurityConfig.java
+++ week-11/securitydemo/src/main/java/com/greenfoxacademy/securitydemo/security/SecurityConfig.java
@@ -1,4 +1,39 @@
 package com.greenfoxacademy.securitydemo.security;
 
-public class SecurityConfig {
+import com.greenfoxacademy.securitydemo.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final CustomUserDetailsService customUserDetailsService;
+
+  @Autowired
+  public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    this.customUserDetailsService = customUserDetailsService;
+  }
+
+  @Bean
+  public static NoOpPasswordEncoder passwordEncoder() {
+    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors().and().csrf().disable().authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/sign_up").permitAll()
+            .antMatchers("/*/floor1/**").hasRole("USER")
+            .antMatchers("/*/floor2/**").hasRole("ADMIN")
+            .and()
+            .httpBasic();
+  }
 }
